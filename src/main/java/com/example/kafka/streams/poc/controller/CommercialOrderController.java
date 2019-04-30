ca8f184b5cd764d5c75bc33916752957febf922e
David Amigo <david.amigo@privalia.com>
2019-04-30 12:12:54 +0200

    Add two new actions in the commercial order controller to show the list and the detail of the commercial orders

--- src/main/java/com/example/kafka/streams/poc/controller/CommercialOrderController.java
+++ src/main/java/com/example/kafka/streams/poc/controller/CommercialOrderController.java
@@ -1,8 +1,12 @@
 package com.example.kafka.streams.poc.controller;
 
 import com.example.kafka.streams.poc.domain.entity.order.CommercialOrder;
+import com.example.kafka.streams.poc.mongodb.entity.CommercialOrderEntity;
+import com.example.kafka.streams.poc.mongodb.repository.CommercialOrderRepository;
 import com.example.kafka.streams.poc.usecase.RandomCommercialOrderProducerUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +15,7 @@ import org.springframework.web.servlet.ModelAndView;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 /**
  * Commercial order controller.
@@ -23,24 +28,34 @@ public class CommercialOrderController {
     /** Use case to produce one or more commercial order with random data */
     private RandomCommercialOrderProducerUseCase commercialOrderProducerUseCase;
 
+    /** The mongoDB repository where to retrieve the commercial orders */
+    private CommercialOrderRepository commercialOrderRepository;
+
     /**
      * Autowired constructor
      *
      * @param commercialOrderProducerUseCase use case
+     * @param commercialOrderRepository the mongoDB commercial order repository
      */
     @Autowired
-    public CommercialOrderController(RandomCommercialOrderProducerUseCase commercialOrderProducerUseCase) {
+    public CommercialOrderController(
+            RandomCommercialOrderProducerUseCase commercialOrderProducerUseCase,
+            CommercialOrderRepository commercialOrderRepository
+    ) {
         this.commercialOrderProducerUseCase = commercialOrderProducerUseCase;
+        this.commercialOrderRepository = commercialOrderRepository;
     }
 
     /**
-     * GET /commercial-order/create
+     * POST /commercial-order/create
+     *
+     * Creates one of more commercial orders with random data
      *
      * @param orderCount the number of commercial orders to create
-     * @return the name of the template to load
+     * @return the model and view
      */
     @PostMapping("/create")
-    public ModelAndView postCreateAction(@RequestParam Integer orderCount) {
+    public ModelAndView postCreateOrdersAction(@RequestParam Integer orderCount) {
         ModelAndView mav  = new ModelAndView("commercial-order/created");
         mav.addObject("orderCount", orderCount);
 
@@ -52,6 +67,60 @@ public class CommercialOrderController {
             exc.printStackTrace();
             mav.addObject("errorText", exc.getMessage());
         }
+
+        return mav;
+    }
+
+    /**
+     * GET /commercial-order
+     *
+     * Lists the commercial orders
+     *
+     * @param size  the page size (default = 15)
+     * @param page  the page number (default = 0)
+     * @return the model and view
+     */
+    @GetMapping({"", "/"})
+    public ModelAndView getOrdersAction(
+            @RequestParam(value="size", required=false, defaultValue="15") int size,
+            @RequestParam(value="page", required=false, defaultValue="0") int page
+    )  {
+        ModelAndView mav  = new ModelAndView("commercial-order/list");
+
+        List<CommercialOrderEntity> commercialOrders = commercialOrderRepository
+                .findAll(PageRequest.of(page, size, new Sort(Sort.Direction.DESC, "datetime")))
+                .getContent();
+
+        long count = commercialOrderRepository.count();
+        long prev = (page > 0) ? page - 1 : 0;
+        long next = (size * (page + 1) < count) ? page + 1 : page;
+
+        mav.addObject("commercialOrders", commercialOrders);
+        mav.addObject("count", count);
+        mav.addObject("size", size);
+        mav.addObject("page", page);
+        mav.addObject("prev", prev);
+        mav.addObject("next", next);
+
+        return mav;
+    }
+
+    /**
+     * GET /commercial-order/{id}
+     *
+     * Shows a commercial order
+     *
+     * @param uuid the uuid of the commercial order
+     * @return the model and view
+     */
+    @GetMapping("/{id}")
+    public ModelAndView getOrdersAction(@PathVariable("id") String uuid) {
+        ModelAndView mav  = new ModelAndView("commercial-order/show");
+
+        Optional<CommercialOrderEntity> commercialOrder = commercialOrderRepository.findById(uuid);
+
+        mav.addObject("uuid", uuid);
+        mav.addObject("commercialOrder", commercialOrder.orElse(null));
 
         return mav;
     }
