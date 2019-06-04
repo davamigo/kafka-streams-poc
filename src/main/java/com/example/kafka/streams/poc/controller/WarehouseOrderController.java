71b6cf518660580907442e936cb7af84e0d5aa82
David Amigo <david.amigo@privalia.com>
2019-06-04 10:25:55 +0200

    Add new controler to see the detail of the failed warehouse order lines

--- src/main/java/com/example/kafka/streams/poc/controller/WarehouseOrderController.java
+++ src/main/java/com/example/kafka/streams/poc/controller/WarehouseOrderController.java
@@ -7,11 +7,13 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Warehouse order controller.
@@ -68,4 +70,25 @@ public class WarehouseOrderController {
 
         return mav;
     }
+
+    /**
+     * GET /warehouse-order/line/failed/{id}
+     *
+     * Shows a failed warehouse order line
+     *
+     * @param uuid the uuid of the warehouse order line
+     * @return the model and view
+     */
+    @GetMapping("/line/failed/{id}")
+    public ModelAndView getOrdersAction(@PathVariable("id") String uuid) {
+        ModelAndView mav  = new ModelAndView("warehouse-order/show-failed-line");
+
+        Optional<WarehouseOrderLineEntity> warehouseOrderLine = warehouseOrderLineRepository.findById(uuid);
+
+        mav.addObject("uuid", uuid);
+        mav.addObject("warehouseOrderLine", warehouseOrderLine.orElse(null));
+
+        return mav;
+    }
+
 }
