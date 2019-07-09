205499f41779b22303400714081bd1277b4d4b89
David Amigo <david.amigo@privalia.com>
2019-07-09 10:16:34 +0200

    Add parameter to application.yml to allow/avoid auto start the Kafka Streams processes

--- src/main/java/com/example/kafka/streams/poc/kafka/config/KafkaStreamsConfig.java
+++ src/main/java/com/example/kafka/streams/poc/kafka/config/KafkaStreamsConfig.java
@@ -40,7 +40,7 @@ public class KafkaStreamsConfig {
      *
      * @return the default configurations for all the Kafka producers
      */
-    Map<String, Object> streamsConfigs() {
+    private Map<String, Object> streamsConfigs() {
         Map<String, Object> props = new HashMap<>();
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, environment.getProperty("spring.kafka.bootstrap-servers"));
         props.put(StreamsConfig.PROCESSING_GUARANTEE_CONFIG, StreamsConfig.EXACTLY_ONCE);
@@ -53,116 +53,115 @@ public class KafkaStreamsConfig {
         props.put(KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG, environment.getProperty("spring.kafka.schema-registry-url"));
         return props;
     }
+    /**
+     * Create custom streams builder factory
+     *
+     * @param applicationId an identifier for the stream processing application.
+     * @return A factory to build the stream process
+     */
+    private StreamsBuilderFactoryBean newCustomStreamsBuilderFactoryBean(String applicationId) {
+
+        Map<String, Object> props = streamsConfigs();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, applicationId);
+
+        StreamsBuilderFactoryBean bean = new StreamsBuilderFactoryBean(new KafkaStreamsConfiguration(props));
+        bean.setAutoStartup(Boolean.parseBoolean(environment.getProperty("spring.kafka.streams.auto-startup", "true")));
+
+        return bean;
+    }
 
     /**
      * Creates a bean for a Kafka Streams process to convert the commercial orders
      *
-     * @return A factory to build the stream
+     * @return A factory to build the stream process
      */
     @Bean("commercialOrderConverterStreamBuilderFactoryBean")
     public StreamsBuilderFactoryBean commercialOrderConverterStreamBuilderFactoryBean() {
-        Map<String, Object> props = streamsConfigs();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "commercialOrderConverterStream");
-        return new StreamsBuilderFactoryBean(new KafkaStreamsConfiguration(props));
+        return newCustomStreamsBuilderFactoryBean("commercialOrderConverterStream");
     }
 
     /**
      * Creates a bean for a Kafka Streams process to split the commercial order lines
      *
-     * @return A factory to build the stream
+     * @return A factory to build the stream process
      */
     @Bean("commercialOrderLineSplitStreamBuilderFactoryBean")
     public StreamsBuilderFactoryBean commercialOrderLineSplitStreamBuilderFactoryBean() {
-        Map<String, Object> props = streamsConfigs();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "commercialOrderLineSplitStream");
-        return new StreamsBuilderFactoryBean(new KafkaStreamsConfiguration(props));
+        return newCustomStreamsBuilderFactoryBean("commercialOrderLineSplitStream");
     }
 
     /**
      * Creates a bean for a Kafka Streams process to generate the purchase order lines
      *
-     * @return A factory to build the stream
+     * @return A factory to build the stream process
      */
     @Bean("purchaseOrderLineAggregatedStreamBuilderFactoryBean")
     public StreamsBuilderFactoryBean purchaseOrderLineAggregatedStreamBuilderFactoryBean() {
-        Map<String, Object> props = streamsConfigs();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "purchaseOrderLineAggregatedStream");
-        return new StreamsBuilderFactoryBean(new KafkaStreamsConfiguration(props));
+        return newCustomStreamsBuilderFactoryBean("purchaseOrderLineAggregatedStream");
     }
 
     /**
      * Creates a bean for a Kafka Streams process to generate the purchase order from the aggregated purchase order
      * lines.
      *
-     * @return A factory to build the stream
+     * @return A factory to build the stream process
      */
     @Bean("purchaseOrderGeneratedStreamBuilderFactoryBean")
     public StreamsBuilderFactoryBean purchaseOrderGeneratedStreamBuilderFactoryBean() {
-        Map<String, Object> props = streamsConfigs();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "purchaseOrderGeneratedStream");
-        return new StreamsBuilderFactoryBean(new KafkaStreamsConfiguration(props));
+        return newCustomStreamsBuilderFactoryBean("purchaseOrderGeneratedStream");
     }
 
     /**
      * Creates a bean for a Kafka Streams process for generating the warehouse order lines from the purchase order lines
      *
-     * @return A factory to build the stream
+     * @return A factory to build the stream process
      */
     @Bean("warehouseOrderLineGeneratorStreamBuilderFactoryBean")
     public StreamsBuilderFactoryBean warehouseOrderLineGeneratorStreamBuilderFactoryBean() {
-        Map<String, Object> props = streamsConfigs();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "warehouseOrderLineGeneratorStream");
-        return new StreamsBuilderFactoryBean(new KafkaStreamsConfiguration(props));
+        return newCustomStreamsBuilderFactoryBean("warehouseOrderLineGeneratorStream");
     }
 
     /**
      * Creates a bean for a Kafka Streams process to match the warehouse order lines with the product legacy id.
      *
-     * @return A factory to build the stream
+     * @return A factory to build the stream process
      */
     @Bean("warehouseOrderLineProductMatcherStreamBuilderFactoryBean")
     public StreamsBuilderFactoryBean warehouseOrderLineProductMatcherStreamBuilderFactoryBean() {
-        Map<String, Object> props = streamsConfigs();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "warehouseOrderLineProductMatcherStream");
-        return new StreamsBuilderFactoryBean(new KafkaStreamsConfiguration(props));
+        return newCustomStreamsBuilderFactoryBean("warehouseOrderLineProductMatcherStream");
     }
 
     /**
      * Creates a bean for a Kafka Streams process to recovery the unmatched warehouse order lines calling an external
      * API to get the product legacy id.
      *
-     * @return A factory to build the stream
+     * @return A factory to build the stream process
      */
     @Bean("warehouseOrderLineProductRecoveryStreamBuilderFactoryBean")
     public StreamsBuilderFactoryBean warehouseOrderLineProductRecoveryStreamBuilderFactoryBean() {
-        Map<String, Object> props = streamsConfigs();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "warehouseOrderLineProductRecoveryStream");
-        return new StreamsBuilderFactoryBean(new KafkaStreamsConfiguration(props));
+        return newCustomStreamsBuilderFactoryBean("warehouseOrderLineProductRecoveryStream");
     }
 
     /**
      * Creates a bean for a Kafka Streams process to merge the matched warehouse order lines and the recovered warehouse
      * order lines streams into one larger stream.
      *
-     * @return A factory to build the stream
+     * @return A factory to build the stream process
      */
     @Bean("warehouseOrderLineMergerStreamBuilderFactoryBean")
     public StreamsBuilderFactoryBean warehouseOrderLineMergerStreamBuilderFactoryBean() {
-        Map<String, Object> props = streamsConfigs();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "warehouseOrderLineMergerStream");
-        return new StreamsBuilderFactoryBean(new KafkaStreamsConfiguration(props));
+        return newCustomStreamsBuilderFactoryBean("warehouseOrderLineMergerStream");
     }
 
     /**
      * Creates a bean for a Kafka Streams process feeding the product-legacy-id topic from the recovered warehouse order
      * lines stream.
      *
-     * @return A factory to build the stream
+     * @return A factory to build the stream process
      */
     @Bean("productLegacyIdFeederStreamBuilderFactoryBean")
     public StreamsBuilderFactoryBean productLegacyIdFeederStreamBuilderFactoryBean() {
-        Map<String, Object> props = streamsConfigs();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "productLegacyIdFeederStream");
-        return new StreamsBuilderFactoryBean(new KafkaStreamsConfiguration(props));
+        return newCustomStreamsBuilderFactoryBean("productLegacyIdFeederStream");
     }
+
 }
